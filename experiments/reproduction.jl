c164e4badbdc833865f61b65b0a2abea35c59aa8
Bake <tbake@klotz.zib.de>
2024-02-18 14:53:49 +0100

    Reran reproduction

--- experiments/reproduction.jl
+++ experiments/reproduction.jl
@@ -9,7 +9,7 @@ struct Reproduction end
 function reproduce(n::Int = 200, tol::T = 1e-9) where T
 
     dims = [5, 10, 50, 100]
-    nmax = n - 1
+    nmax = n 
     b    = multiple_rhs(dims, n)
     
     spd    = Experiment(dims, n, nmax, SymInstance,    Laplace,  TensorLanczosReorth, b)
